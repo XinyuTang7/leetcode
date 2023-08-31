a731fffb0e01baf3396493b60faea9afb9992e46
XT20685 <xinyu.tang@clpsglobal.com>
2023-08-31 16:25:31 +0800

    字母异位词分组

---
+++ src/algorithm/array/GroupingOfAllogramWords.java
@@ -0,0 +1,41 @@
+package algorithm.array;
+
+import java.util.*;
+
+public class GroupingOfAllogramWords {
+    /**
+     * 给你一个字符串数组，请你将 字母异位词 组合在一起。可以按任意顺序返回结果列表。
+     * <p>
+     * 字母异位词 是由重新排列源单词的所有字母得到的一个新单词。
+     * <p>
+     * 示例 1:
+     * <p>
+     * 输入: strs = ["eat", "tea", "tan", "ate", "nat", "bat"]
+     * 输出: [["bat"],["nat","tan"],["ate","eat","tea"]]
+     *
+     * @param strs
+     * @return
+     */
+    public static List<List<String>> groupAnagrams(String[] strs) {
+        Map<String, List<String>> map = new HashMap<>();
+        for (String str : strs) {
+            char[] chars = str.toCharArray();
+            Arrays.sort(chars);
+            String key = new String(chars);
+            if (map.containsKey(key)) {
+                List<String> list = map.get(key);
+                list.add(str);
+            } else {
+                List<String> list = new ArrayList<>();
+                list.add(str);
+                map.put(key, list);
+            }
+        }
+        return new ArrayList<>(map.values());
+    }
+
+    public static void main(String[] args) {
+        String[] strs = new String[]{"eat", "tea", "tan", "ate", "nat", "bat"};
+        System.out.println(groupAnagrams(strs));
+    }
+}
