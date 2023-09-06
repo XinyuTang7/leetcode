17bd80dd02fac0ebd6f6815b8d29e37ea49187b9
XT20685 <xinyu.tang@clpsglobal.com>
2023-09-06 10:18:10 +0800

    commit

---
+++ src/algorithm/combination/MergingInterval.java
@@ -0,0 +1,30 @@
+package algorithm.combination;
+
+public class MergingInterval {
+
+    /**
+     * 以数组 intervals 表示若干个区间的集合，其中单个区间为 intervals[i] = [starti, endi] 。请你合并所有重叠的区间，并返回 一个不重叠的区间数组，该数组需恰好覆盖输入中的所有区间 。
+     *
+     * 示例 1：
+     * 输入：intervals = [[1,3],[2,6],[8,10],[15,18]]
+     * 输出：[[1,6],[8,10],[15,18]]
+     * 解释：区间 [1,3] 和 [2,6] 重叠, 将它们合并为 [1,6].
+     *
+     * 示例 2：
+     * 输入：intervals = [[1,4],[4,5]]
+     * 输出：[[1,5]]
+     * 解释：区间 [1,4] 和 [4,5] 可被视为重叠区间。
+     * @param intervals
+     * @return
+     */
+    public static int[][] merge(int[][] intervals) {
+        int R = intervals.length;
+        int C = intervals[0].length;
+        int[][] res = new int[R][C];
+        return res;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
